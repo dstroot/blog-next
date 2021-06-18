b601fd18e6011028f7b26c26964111bffa1d0251
Dan Stroot <dan.stroot@gmail.com>
2021-06-18 11:54:09 -0700

    tweaking dark mode

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
-        <body className="dark:bg-black">
+        <body className="dark:bg-black dark:text-white">
           <Main />
           <NextScript />
         </body>
