45b419199b3276a7cad4d00dbbac0b6ba30f37e8
Dan Stroot <dan.stroot@gmail.com>
2021-06-24 15:00:29 -0700

    added posts

--- components/intro.js
+++ components/intro.js
@@ -4,13 +4,13 @@ import { ThemeChanger } from "./ThemeChanger";
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+      <p className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Blog.
-      </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
+      </p>
+      <p className="text-center md:text-left mt-5 md:pl-8 text-xl">
         <ThemeChanger />
         {CMS_NAME}. {TAGLINE}.
-      </h4>
+      </p>
     </section>
   );
 }
