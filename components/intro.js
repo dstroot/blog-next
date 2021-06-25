3c99c26e9236afd8106d94edf8aef642cc38ecc9
Dan Stroot <dan.stroot@gmail.com>
2021-06-25 13:28:25 -0700

    changed header

--- components/intro.js
+++ components/intro.js
@@ -3,14 +3,18 @@ import { ThemeChanger } from "./ThemeChanger";
 
 export default function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <p className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
-      </p>
-      <p className="text-center md:text-left mt-5 md:pl-8 text-xl">
+    <>
+      <div className="mt-4">
         <ThemeChanger />
-        {CMS_NAME}. {TAGLINE}.
-      </p>
-    </section>
+      </div>
+      <section className="flex-col md:flex-row flex items-center md:justify-between md:mt-10 mb-10 md:mb-12">
+        <p className="text-7xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+          Blog.
+        </p>
+        <p className="text-center md:text-left mt-1 md:pl-8 text-xl">
+          {CMS_NAME}. {TAGLINE}.
+        </p>
+      </section>
+    </>
   );
 }
