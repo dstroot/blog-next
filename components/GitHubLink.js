96aa72c92d535b1431ff313cc1defd17de547b4a
Dan Stroot <dan.stroot@gmail.com>
2021-06-26 12:43:46 -0700

    updated edit link

--- components/GitHubLink.js
+++ components/GitHubLink.js
@@ -1,8 +1,8 @@
 export const GitHubLink = ({ path }) => {
   return (
     <div className="max-w-2xl mx-auto mt-6">
-      <a href={path} className="hover:underline text-sm text-gray-400">
-        &lt; Edit on GitHub &gt;
+      <a href={path} className="hover:underline text-sm text-gray-500">
+        &lt; edit post &gt;
       </a>
     </div>
   );
