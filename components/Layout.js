d11c24651a2d2921d43b7d930dbd852f63765ac3
Dan Stroot <dan.stroot@gmail.com>
2021-06-25 21:46:29 -0700

    fix Meta again

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,13 @@
-// import Alert from "../components/alert";
+import Alert from "../components/alert";
 import { Footer } from "./Footer";
-import { Meta } from "./Meta2";
+import { Meta } from "./Meta";
 
 export const Layout = ({ children }) => {
   return (
     <>
       <Meta />
       <div className="min-h-screen">
-        {/* <Alert alert={alert} /> */}
+        <Alert alert={alert} />
         <main>{children}</main>
       </div>
       <Footer />
