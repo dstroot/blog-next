8a3ad9fd9a4e9b113875d812e9f50f2e14731019
Dan Stroot <dan.stroot@gmail.com>
2021-06-25 20:44:52 -0700

    code cleanup

--- components/intro.js
+++ components/intro.js
@@ -3,18 +3,16 @@ import { ThemeChanger } from "./ThemeChanger";
 
 export const Intro = () => {
   return (
-    <>
-      <div className="mt-4 flex-none">
-        <ThemeChanger />
-      </div>
-      <section className="flex flex-col md:flex-row items-center md:justify-between md:mt-10 mb-10 md:mb-12">
+    <section className="mt-4 md:mt-10 mb-10 md:mb-12">
+      <ThemeChanger />
+      <div className="flex flex-col md:flex-row items-center md:justify-between">
         <p className="text-7xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
           Blog.
         </p>
         <p className="text-center md:text-left mt-1 md:pl-8 font-semibold italic md:text-2xl">
           {CMS_NAME}. {TAGLINE}.
         </p>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
