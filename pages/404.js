6f9d3897a1d735eda186a019aef0b28931a9f16e
Dan Stroot <dan@thestroots.com>
2021-06-29 10:46:56 -0700

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -8,11 +8,11 @@ export default function Custom404() {
           <a className="text-2xl md:text-4xl hover:underline">{"< Home."}</a>
         </Link>
       </div>
-      <div className="mt-20 flex flow-col justify-center items-center">
-        <div className="text-4xl md:text-8xl font-bold tracking-tighter leading-tight border-r-4 pr-3 md:pr-6">
+      <div className="mt-20 flex flex-col md:flex-row justify-center items-center">
+        <div className="text-8xl font-bold tracking-tighter leading-tight border-b-4 md:border-r-4 pb-3 md:pr-6">
           404
         </div>
-        <div className="font-semibold italic text-base md:text-2xl pl-3 md:pl-6">
+        <div className="font-semibold italic text-xl md:text-2xl pt-3 md:pl-6">
           This page could not be found.
         </div>
       </div>
