bc3b7dfd88097809c4974279da24da3ef82925c9
Dan Stroot <dan.stroot@gmail.com>
2021-06-29 22:20:01 -0700

    fix theme

--- components/PostTitle.js
+++ components/PostTitle.js
@@ -1,6 +1,6 @@
 export const PostTitle = ({ children }) => {
   return (
-    <h1 className="text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
+    <h1 className="text-5xl md:text-7xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
       {children}
     </h1>
   );
