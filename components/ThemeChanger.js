9cb89319c38725d2de885830f16b785b6ed584af
Dan Stroot <dan.stroot@gmail.com>
2021-06-29 19:12:28 -0700

    customization

--- components/ThemeChanger.js
+++ components/ThemeChanger.js
@@ -34,7 +34,7 @@ export const ThemeChanger = () => {
         setTheme(theme === "light" ? "dark" : "light");
       }}
     >
-      {theme === "dark" ? (
+      {theme === "light" ? (
         <>
           <span className="sr-only">Light Mode</span>
           <svg
