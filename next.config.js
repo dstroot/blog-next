d3ecc9a9d95e16bd244b21be698e844b7ebcc6f4
Dan Stroot <dan.stroot@gmail.com>
2021-07-04 16:29:28 -0700

    new posts

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  poweredByHeader: false,
+  images: {
+    // domains: ["assets.vercel.com"],
+    // These widths are used when the next/image component uses layout="responsive"
+    // or layout="fill" so that the correct image is served for the device visiting your website.
+    deviceSizes: [335, 556, 684, 728, 984, 1240, 1496],
+  },
+};
