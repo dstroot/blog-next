4225f407299137f411381572d8ced4f9daad6777
Dan Stroot <dan.stroot@gmail.com>
2021-07-07 20:53:04 -0700

    updated post

--- next.config.js
+++ next.config.js
@@ -6,4 +6,14 @@ module.exports = {
     // or layout="fill" so that the correct image is served for the device visiting your website.
     deviceSizes: [335, 556, 684, 728, 984, 1240, 1496],
   },
+  // https://nextjs.org/docs/api-reference/next.config.js/redirects
+  async redirects() {
+    return [
+      {
+        source: "/(\\d{1,})/(\\d{1,})/(\\d{1,})/:slug",
+        destination: "/posts/:slug", // Matched parameters can be used in the destination
+        permanent: false,
+      },
+    ];
+  },
 };
