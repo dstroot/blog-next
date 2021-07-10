e67e7fc8016b83392abdb326aeec68b46853b517
Dan Stroot <dan.stroot@gmail.com>
2021-07-10 13:33:32 -0700

    updated redirects

--- next.config.js
+++ next.config.js
@@ -4,14 +4,21 @@ module.exports = {
     // domains: ["assets.vercel.com"],
     // These widths are used when the next/image component uses layout="responsive"
     // or layout="fill" so that the correct image is served for the device visiting your website.
-    deviceSizes: [335, 556, 684, 728, 984, 1240, 1496],
+    // deviceSizes: [335, 556, 684, 728, 984, 1240, 1496],
   },
   // https://nextjs.org/docs/api-reference/next.config.js/redirects
   async redirects() {
     return [
+      // this one just trims the old date paths
+      // {
+      //   source: "/(\\d{1,})/(\\d{1,})/(\\d{1,})/:slug",
+      //   destination: "/posts/:slug", // Matched parameters can be used in the destination
+      //   permanent: false,
+      // },
+      // this rebuilds the path to match the new format
       {
-        source: "/(\\d{1,})/(\\d{1,})/(\\d{1,})/:slug",
-        destination: "/posts/:slug", // Matched parameters can be used in the destination
+        source: "/:yr(\\d{1,})/:mo(\\d{1,})/:d(\\d{1,})/:slug",
+        destination: "/posts/:yr-:mo-:d-:slug", // Matched parameters can be used in the destination
         permanent: false,
       },
     ];
