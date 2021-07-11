dd212b5ef5a0fc0d84c44b7f3ee1fe61fff63781
Dan Stroot <dan.stroot@gmail.com>
2021-07-11 08:17:27 -0700

    added list

--- components/List.js
+++ components/List.js
@@ -1,33 +1,42 @@
-import { PostPreview } from "./PostPreview";
+import { parseISO } from "date-fns";
+import Link from "next/link";
+import { DateFormatter } from "./DateFormatter";
 
 export const List = ({ posts }) => {
+  let year, newYear, changed;
+
   return (
     <>
       <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
-        Posts
+        Archive
       </h2>
-
-      {posts.map((post, index) => {
-        return (
-          <div key={`${post.id}-${index}`}>
-            <p>
-              {post.title}, {post.date}
-            </p>
-            {/* <PostPreview
-                key={post.slug}
-                title={post.title}
-                coverImage={post.coverImage}
-                date={post.date}
-                author={post.author}
-                slug={post.slug}
-                excerpt={post.excerpt}
-                stats={post.stats}
-                // fileName={post.fileName}
-                onIsVisible={() => onIsVisible(index)}
-              /> */}
-          </div>
-        );
-      })}
+      <div className="max-w-2xl mx-auto mb-8">
+        {posts.map((post, index) => {
+          const postDate = parseISO(post.date);
+          newYear = postDate.getFullYear();
+          if (year != newYear) {
+            year = newYear;
+            changed = true;
+          } else {
+            changed = false;
+          }
+          return (
+            <div key={`${post.id}-${index}`}>
+              {changed && (
+                <h3 className="mb-2 mt-4 text-xl md:text-2xl font-bold tracking-tighter leading-tight">
+                  {year}
+                </h3>
+              )}
+              <Link as={`/posts/${post.slug}`} href="/posts/[slug]">
+                <a className="ml-10 text-xl hover:underline">{post.title}</a>
+              </Link>
+              <span className="text-gray-500 float-right">
+                <DateFormatter dateString={post.date} />
+              </span>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
