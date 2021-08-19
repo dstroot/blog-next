41bb8b014f23beeb04255c7e410d940a48bad7f7
Dan Stroot <dan.stroot@gmail.com>
2021-08-19 12:46:16 -0700

    updated dependencies

--- components/DarkMode.js
+++ components/DarkMode.js
@@ -3,38 +3,21 @@ import { useTheme } from 'next-themes';
 
 export const DarkMode = () => {
   const [mounted, setMounted] = useState(false);
-  const { resolvedTheme, theme, setTheme } = useTheme();
-  const [selected, setSelected] = useState('auto');
+  const { theme, setTheme } = useTheme();
 
   const divStyle = {
-    height: '15px',
-    width: '30px',
+    height: '22px',
+    width: '130px',
   };
 
   // When mounted on client we can show the UI
   useEffect(() => setMounted(true), []);
 
-  // if theme is 'system' change it to the resolved theme
-  // because tailwind needs body class to be "light" or "dark"
-  // useEffect(() => {
-  //   if (mounted) {
-  //     if (resolvedTheme === undefined && theme === undefined) {
-  //       setTheme('light'); // light is default
-  //     }
-  //     if (theme === 'system') {
-  //       setTheme(resolvedTheme);
-  //     }
-  //   }
-  // }, [resolvedTheme, theme, setTheme, mounted]);
-
   // if not mounted return a placeholder
   if (!mounted) return <div style={divStyle}> </div>;
 
-  // When mounted on client show the UI
-  // <div className={"w-full flex-grow lg:flex lg:items-center lg:w-auto " + (selected ? 'bg-blue-600 hover:bg-blue-800 text-white' : 'text-blue-600')}></div>
-  // flex flex-row justify-center
   return (
-    <div className='inline-flex rounded-full border border-blue-500 p-0.5 text-center tracking-tight text-xs m-2'>
+    <div className='inline-flex rounded-full border border-blue-500 p-0.5 text-center tracking-tight text-xs'>
       <div
         className={
           'flex-1 rounded-full transition duration-100 hover:cursor-pointer px-2' +
@@ -42,10 +25,7 @@ export const DarkMode = () => {
             ? ' bg-blue-600  text-white'
             : ' text-blue-600 font-light')
         }
-        onClick={() => {
-          setSelected('light');
-          setTheme('light');
-        }}
+        onClick={() => setTheme('light')}
       >
         Light
       </div>
@@ -56,10 +36,7 @@ export const DarkMode = () => {
             ? ' bg-blue-600  text-white'
             : ' text-blue-600 font-light')
         }
-        onClick={() => {
-          setSelected('system');
-          setTheme('system');
-        }}
+        onClick={() => setTheme('system')}
       >
         Auto
       </div>
@@ -70,10 +47,7 @@ export const DarkMode = () => {
             ? ' bg-blue-600  text-white'
             : ' text-blue-600 font-light')
         }
-        onClick={() => {
-          setSelected('dark');
-          setTheme('dark');
-        }}
+        onClick={() => setTheme('dark')}
       >
         Dark
       </div>
