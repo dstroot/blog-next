f8d8a23b7132731466f9c7ef08dd2fb40d3fab18
Dan Stroot <dan.stroot@gmail.com>
2021-08-21 16:55:18 -0700

    updated post width

--- components/PostBody.js
+++ components/PostBody.js
@@ -3,9 +3,9 @@ import { GitHubLink } from './GitHubLink';
 export const PostBody = ({ content, path }) => {
   return (
     <>
-      <div className='max-w-2xl mx-auto'>
+      <div className='max-w-3xl mx-auto'>
         <div
-          className='prose prose-lg dark:prose-dark max-w-none'
+          className='prose prose-lg dark:prose-dark max-w-none break-words'
           dangerouslySetInnerHTML={{ __html: content }}
         />
       </div>
