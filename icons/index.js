c7e19f7566f2eedb6dec35ec99637e988d95519b
Dan Stroot <dan@thestroots.com>
2021-11-04 13:04:28 -0700

    snippets

---
+++ icons/index.js
@@ -0,0 +1,22 @@
+import { GiEarthAfricaEurope } from 'react-icons/gi';
+import { SiGoogleanalytics, SiTailwindcss } from 'react-icons/si';
+import { BsMarkdownFill } from 'react-icons/bs';
+import { CgTimelapse, CgFramer } from 'react-icons/cg';
+
+export const icons = {
+  opgp: <GiEarthAfricaEurope />,
+  ganalytics: <SiGoogleanalytics />,
+  mdx: <BsMarkdownFill />,
+  timeout: <CgTimelapse />,
+  fm: <CgFramer />,
+  tailwind: <SiTailwindcss />,
+};
+
+// https://react-icons.github.io/react-icons/
+
+// IoLogoJavascript;
+// IoLogoReact;
+// IoLogoHtml5;
+// IoLogoCss3;
+// SiTailwindcss;
+// VscTerminalBash;
