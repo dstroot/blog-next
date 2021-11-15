b68a72c55e99e01f4fbe5ea1d23fc18e06379e37
Dan Stroot <dan@thestroots.com>
2021-11-14 19:54:58 -0800

    snippets

--- pages/snippets/[slug].js
+++ pages/snippets/[slug].js
@@ -14,18 +14,15 @@ export default function BlogSlug({ code, frontMatter }) {
     <Container>
       <article className='mx-auto max-w-3xl my-6 md:my-10'>
         <div className='flex flex-col justify-between items-center md:flex-row mb-8'>
-          <h2 className='order-2 my-3  text-4xl md:text-6xl lg:text-7xl tracking-tighter leading-tight md:leading-none font-black text-center md:order-1 text-slate-light md:my-0'>
+          <h2 className='order-2 my-3  text-3xl md:text-4xl lg:text-5xl tracking-tighter leading-tight md:leading-none font-black text-center md:order-1 text-slate-light md:my-0'>
             {frontMatter.title}
           </h2>
-          {/* <PostTitle>{frontMatter.title}</PostTitle> */}
           <div className='order-1 md:order-2 my-3 md:my-0 w-16 h-16 rounded-full bg-gray-300'>
-            <span className='grid place-items-center w-full h-full text-black text-3xl'>
+            <span className='grid place-items-center w-full h-full text-blue-800 text-3xl'>
               {icons[frontMatter.icon]}
             </span>
           </div>
         </div>
-        {/* <div className='text-3xl text-blue-700'>{icons[frontMatter.icon]}</div>
-        <PostTitle>{frontMatter.title}</PostTitle> */}
         <p className='text-xl text-gray-500 mb-4'>{frontMatter.summary}</p>
         <div className='flex space-x-2 text-xs mb-8'>
           <p className='px-3 py-1 text-sm bg-gray-100 dark:bg-gray-700 rounded-full'>
