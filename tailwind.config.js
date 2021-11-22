9a0953e4cec4ee5d5da54ca5dd4450fef7c3e9da
Dan Stroot <dan@thestroots.com>
2021-11-22 11:57:50 -0800

    snippet cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ const colors = require('tailwindcss/colors');
 module.exports = {
   reactStrictMode: true,
   experimental: {
-    concurrentFeatures: false,
-    serverComponents: false,
+    concurrentFeatures: true,
+    serverComponents: true,
     optimizeCss: true,
     esmExternals: true,
   },
