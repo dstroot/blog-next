89cd730e56f48497048e5d5112120b9c6bd29844
Dan Stroot <dan@thestroots.com>
2021-12-02 10:39:22 -0800

    tweaked stats component

--- components/Stats.js
+++ components/Stats.js
@@ -10,24 +10,24 @@ export const Stats = () => {
       <h2 className='flex items-center py-5 space-x-2 text-2xl font-bold'>
         {/* <span className='font-mono text-base'>01.</span> */}
         <span className=''>Google Analytics Stats</span>
-        <span className='w-20 h-[1px] bg-white'></span>
+        <span className='w-20 h-[1px] bg-black dark:bg-white'></span>
       </h2>
 
-      <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
+      <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 mb-6'>
         {/* Items */}
-        <div className='flex relative flex-col items-center p-5 space-y-5 w-full rounded shadow-lg dark:bg-gray-800'>
+        <div className='flex relative flex-col items-center p-5 space-y-5 w-full rounded shadow-lg bg-gray-100 dark:bg-gray-800'>
           <h1 className='text-2xl font-black tracking-wide'>Page Views</h1>
           <span className='text-5xl font-black'>{!stats ? '-' : millify(stats.pageViews)}</span>
         </div>
         {/* Items */}
-        <div className='flex flex-col items-center p-5 space-y-5 w-full rounded shadow-lg dark:bg-gray-800'>
+        <div className='flex flex-col items-center p-5 space-y-5 w-full rounded shadow-lg bg-gray-100 dark:bg-gray-800'>
           <h1 className='text-2xl font-black tracking-wide'>Engagement Rate</h1>
           <span className='text-5xl font-black'>
             {!stats ? '-' : (stats.engagmentRate * 100).toFixed(0) + '%'}
           </span>
         </div>
         {/* Items */}
-        <div className='flex flex-col items-center p-5 space-y-5 w-full rounded shadow-lg dark:bg-gray-800'>
+        <div className='flex flex-col items-center p-5 space-y-5 w-full rounded shadow-lg bg-gray-100 dark:bg-gray-800'>
           <h1 className='text-2xl font-black tracking-wide'>Total Users</h1>
           <span className='text-5xl font-black'>{!stats ? '-' : millify(stats.totalUsers)}</span>
         </div>
