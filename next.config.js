a0d426a8eb81f8aea42836da792da16fd3c1beb6
Dan Stroot <dan@thestroots.com>
2021-12-07 14:52:30 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,13 +5,9 @@ module.exports = {
     concurrentFeatures: true,
     serverComponents: true,
   },
-  // images: {
-  //   formats: ['image/avif', 'image/webp'],
-  //   // domains: ["assets.vercel.com"],
-  //   // These widths are used when the next/image component uses layout="responsive"
-  //   // or layout="fill" so that the correct image is served for the device visiting your website.
-  //   // deviceSizes: [335, 556, 684, 728, 984, 1240, 1496],
-  // },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
   // https://nextjs.org/docs/api-reference/next.config.js/redirects
   async redirects() {
     return [
