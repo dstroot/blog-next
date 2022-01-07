a0b349830128d036c05a0ca32edd250021670daa
Dan Stroot <dan@thestroots.com>
2022-01-06 18:50:09 -0800

    added a csp report uri

--- pages/api/csp/index.js
+++ pages/api/csp/index.js
@@ -1,5 +1,5 @@
 import { upd } from '../../../lib/dynamodb';
-import uuidv4 from 'uuid/v4';
+import { v4 as uuidv4 } from 'uuid';
 
 export default async function handler(req, res) {
   let params = { TableName: csp_reports };
