fd08127d4a014cbe365efbecb053ee7bcd030ce1
Dan Stroot <dan@thestroots.com>
2022-02-27 19:48:06 -0800

    new post

--- components/CommandPalette.jsx
+++ components/CommandPalette.jsx
@@ -44,7 +44,7 @@ export const CommandPalette = () => {
           leaveFrom='opacity-100'
           leaveTo='opacity-0'
         >
-          <Dialog.Overlay className='fixed inset-0 bg-gray-300/75' />
+          <Dialog.Overlay className='fixed inset-0 bg-gray-300/75 dark:bg-gray-800/75' />
         </Transition.Child>
         <Transition.Child
           enter=' ease-out duration-300'
@@ -60,7 +60,7 @@ export const CommandPalette = () => {
               setIsOpen(false);
             }}
             as='div'
-            className='relative max-w-xl mx-auto overflow-hidden bg-white divide-y-2 divide-gray-100 shadow-2xl rounded-xl ring-1 ring-black/5'
+            className='relative max-w-xl mx-auto overflow-hidden bg-white divide-y-2 divide-gray-100 shadow-2xl dark:divide-gray-800 dark:bg-black rounded-xl ring-1 ring-black/5'
           >
             <div className='flex items-center px-4 py-2'>
               <svg
@@ -72,7 +72,7 @@ export const CommandPalette = () => {
                 <path d='M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z' />
               </svg>
               <Combobox.Input
-                className='w-full h-12 ml-2 text-gray-800 placeholder-gray-400 bg-transparent border-0 focus:ring-0'
+                className='w-full h-12 ml-2 text-gray-800 placeholder-gray-600 bg-transparent border-0 dark:placeholder-gray-300 dark:text-gray-200 focus:ring-0'
                 placeholder='Search...'
                 onChange={(event) => setQuery(event.target.value)}
               />
@@ -86,7 +86,9 @@ export const CommandPalette = () => {
                     {({ active, selected }) => (
                       <li
                         className={`px-4 py-2 ${
-                          active ? 'bg-blue-500 text-white' : 'bg-white text-black'
+                          active
+                            ? 'bg-blue-500 dark:bg-blue-800 text-white'
+                            : 'bg-white text-black dark:bg-black dark:text-gray-200'
                         }`}
                       >
                         {/*selected && <CheckIcon />*/}
