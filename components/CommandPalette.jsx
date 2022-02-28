cbe8ea40d6e66b4485e74596ec88aa3d229c3957
Dan Stroot <dan@thestroots.com>
2022-02-28 13:42:39 -0800

    blur search background

--- components/CommandPalette.jsx
+++ components/CommandPalette.jsx
@@ -37,20 +37,20 @@ export const CommandPalette = () => {
     <Transition.Root show={isOpen} as={Fragment} afterLeave={() => setQuery('')}>
       <Dialog onClose={setIsOpen} className='fixed inset-0 p-4 pt-[15vh] overflow-y-auto'>
         <Transition.Child
-          enter='ease-out duration-300'
+          enter='ease-out duration-200'
           enterFrom='opacity-0'
           enterTo='opacity-100'
-          leave='ease-in duration-200'
+          leave='ease-in duration-150'
           leaveFrom='opacity-100'
           leaveTo='opacity-0'
         >
-          <Dialog.Overlay className='fixed inset-0 bg-gray-300/75 dark:bg-gray-800/75' />
+          <Dialog.Overlay className='fixed inset-0 bg-gray-300/75 dark:bg-gray-800/75 backdrop-blur-sm' />
         </Transition.Child>
         <Transition.Child
-          enter=' ease-out duration-300'
+          enter=' ease-out duration-200'
           enterFrom='opacity-0 scale-95'
           enterTo='opacity-100 scale-100'
-          leave=' ease-in duration-200'
+          leave=' ease-in duration-150'
           leaveFrom='opacity-100 scale-100'
           leaveTo='opacity-0 scale-95'
         >
