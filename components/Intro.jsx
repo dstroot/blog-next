85451bbd3d000e83e2afcfe82b4685d7bdf4a173
Dan Stroot <dan@thestroots.com>
2022-05-01 07:14:49 -0700

    new post

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -8,7 +8,6 @@ export const Intro = () => {
         <ThemeChanger />
         <div className='flex flex-col items-center md:flex-row md:justify-between'>
           <p className='text-6xl font-bold leading-tight tracking-tighter md:text-7xl'>
-            {/* Blog */}
             {CMS_NAME + ' · Blog'}
           </p>
           <p className='mt-2 text-xl italic font-semibold md:text-2xl'>{TAGLINE}.</p>
