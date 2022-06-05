c164bfa69ebe7a764e0ec23ff067586e4fabcda7
Dan Stroot <dan@thestroots.com>
2022-06-05 12:40:56 -0700

    sitemap config

--- components/MDXComponents/CustomImage.jsx
+++ components/MDXComponents/CustomImage.jsx
@@ -1,8 +1,11 @@
+import { cn } from '../../lib/utils';
+import { useState } from 'react';
 import Image from 'next/image';
 
 export const CustomImage = (props) => {
   const src = props.src;
   const isInternalImage = src && src.startsWith('/');
+  const [isLoading, setLoading] = useState(true);
 
   if (isInternalImage) {
     return (
@@ -10,7 +13,16 @@ export const CustomImage = (props) => {
       // so they are automatically passed here with {...props}
       // https://www.peterlunch.com/snippets/next-image-styling
       // eslint-disable-next-line jsx-a11y/alt-text
-      <Image {...props} layout='responsive' loading='lazy' />
+      <Image
+        {...props}
+        layout='responsive'
+        loading='lazy'
+        className={cn(
+          'duration-500 ease-in-out group-hover:opacity-75',
+          isLoading ? 'scale-110 blur-2xl grayscale' : 'scale-100 blur-0 grayscale-0'
+        )}
+        onLoadingComplete={() => setLoading(false)}
+      />
     );
   }
 
