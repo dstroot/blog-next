08447ea28e520a045ad0bb80c3c4a1dfbfc69514
Dan Stroot <dan@thestroots.com>
2022-10-21 16:34:42 -0700

    updated dependencies

--- pages/api/ogimage/index.js
+++ pages/api/ogimage/index.js
@@ -1,5 +1,6 @@
 import { ImageResponse } from '@vercel/og';
-
+import { CMS_NAME, TAGLINE } from '../../../lib/constants';
+// import author from '../assets/blog/authors/dan.jpeg';
 // see: https://og-playground.vercel.app/ to test images
 // docs: https://vercel.com/docs/concepts/functions/edge-functions/og-image-generation
 
@@ -7,27 +8,48 @@ export const config = {
   runtime: 'experimental-edge',
 };
 
-export default function ogimage() {
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 128,
-          background: 'white',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          textAlign: 'center',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        Hello world!
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-    }
-  );
+export default async function ogimage() {
+  try {
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            backgroundColor: 'white',
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <div tw='flex'>
+            <img
+              width='256'
+              height='256'
+              src={`https://www.danstroot.com/_next/image?url=%2Fassets%2Fblog%2Fauthors%2Fdan.jpeg&w=640&q=90`}
+              style={{
+                borderRadius: 128,
+                marginRight: 75,
+              }}
+            />
+
+            <div tw='flex flex-col'>
+              <p tw='font-bold leading-tight tracking-tighter text-7xl'>{CMS_NAME + ' · Blog'}</p>
+              <p tw='mt-2 ml-2 italic font-semibold text-3xl'>{TAGLINE}.</p>
+            </div>
+          </div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      }
+    );
+  } catch (e) {
+    console.log(`${e.message}`);
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
 }
