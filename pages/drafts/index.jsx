f6f331a552bcecfbc08ea5f2c037afba62a1466b
Dan Stroot <dan@thestroots.com>
2022-11-25 08:39:41 -0800

    updated dependencies

--- pages/drafts/index.jsx
+++ pages/drafts/index.jsx
@@ -1,11 +1,9 @@
 import { SEO } from '../../components/Seo';
 import { Intro } from '../../components/Intro';
-import { Header } from '../../components/Header';
 import { HeroPost } from '../../components/HeroPost';
 import { Container } from '../../components/Container';
 import { SortByDate } from '../../lib/sortPosts';
 import { MoreStories } from '../../components/MoreStories';
-import { generateRSSFeed } from '../../lib/feed';
 import { getAllFilesFrontMatter } from '../../lib/getAllFiles';
 import { CMS_NAME, BASE_URL, HOME_OG_IMAGE_URL } from '../../lib/constants';
 
@@ -23,17 +21,16 @@ export default function Index({ posts }) {
     twHandle: '@danstroot',
   };
 
-  if (process.env.NODE_ENV === 'production') {
-    return (
-      <>
-        <SEO {...seo} />
-        <Container>
-          <Header />
-          <h3 class='mb-4 text-3xl leading-tight md:text-4xl'>No Drafts Found.</h3>
-        </Container>
-      </>
-    );
-  }
+  // if (process.env.NODE_ENV === 'production') {
+  //   return (
+  //     <>
+  //       <SEO {...seo} />
+  //       <Container>
+  //         <h3 class='mb-4 text-3xl leading-tight md:text-4xl'>No Drafts Found.</h3>
+  //       </Container>
+  //     </>
+  //   );
+  // }
 
   return (
     <>
@@ -60,14 +57,11 @@ export default function Index({ posts }) {
 export async function getStaticProps() {
   let posts = await getAllFilesFrontMatter('data/_posts', '.mdx');
 
-  // Remove any unpublished posts
+  // filter for drafts
   posts = posts.filter((posts) => !posts.published);
 
   // Sort list by published date
   posts = SortByDate(posts);
-
-  // Build rss feed when site builds
-  await generateRSSFeed(posts);
 
   return { props: { posts } };
 }
