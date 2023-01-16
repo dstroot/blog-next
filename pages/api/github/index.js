8f10b222f2a07e9d41481210af0784f654f29653
Dan Stroot <dan@thestroots.com>
2023-01-16 08:57:49 -0800

    updated dependencies

--- pages/api/github/index.js
+++ pages/api/github/index.js
@@ -1,6 +1,6 @@
-export const config = {
-  runtime: 'edge',
-};
+// export const config = {
+//   runtime: 'edge',
+// };
 
 export default async function handler(req, res) {
   const results = await fetch(encodeURI(process.env.GITHUB_API)).then((res) => res.json());
