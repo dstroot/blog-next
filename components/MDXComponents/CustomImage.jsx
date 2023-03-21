5a833f785c5f3004f112ceb0fb0321134dc0544a
Dan Stroot <dan.stroot@gmail.com>
2023-03-20 19:19:08 -0700

    updated dependencies

--- components/MDXComponents/CustomImage.jsx
+++ components/MDXComponents/CustomImage.jsx
@@ -11,6 +11,7 @@ export const CustomImage = (props) => {
     return (
       // height and width are part of the props, from rehype-img-size
       // so they are automatically passed here with {...props}
+
       // https://www.peterlunch.com/snippets/next-image-styling
 
       // eslint-disable-next-line jsx-a11y/alt-text
--- pages/api/subscribers/index.js
+++ pages/api/subscribers/index.js
@@ -1,13 +1,13 @@
-import client from '@mailchimp/mailchimp_marketing';
-// https://github.com/mailchimp/mailchimp-marketing-node
-
 export default async function handler(req, res) {
-  client.setConfig({
-    apiKey: process.env.MAILCHIMP_API_KEY,
-    server: 'us20',
-  });
+  const url = encodeURI(
+    process.env.NEXT_PUBLIC_MAILCHIMP_URL + '/lists/' + process.env.MAILCHIMP_LIST_ID
+  );
+  const options = {
+    headers: {
+      Authorization: 'Bearer ' + process.env.MAILCHIMP_API_KEY,
+    },
+  };
 
-  const response = await client.lists.getAllLists();
-  // console.log(response);
-  return res.status(200).json(response.lists[0].stats.member_count);
+  const results = await fetch(url, options).then((res) => res.json());
+  return res.status(200).json(results.stats.member_count);
 }
