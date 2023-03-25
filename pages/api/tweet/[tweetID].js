be511a04dfcb5723dbe4043507798bea66e3c886
Dan Stroot <dan.stroot@gmail.com>
2023-03-25 07:22:25 -0700

    updated dependencies

--- pages/api/tweet/[tweetID].js
+++ pages/api/tweet/[tweetID].js
@@ -1,6 +1,6 @@
-export const config = {
-  runtime: 'edge',
-};
+// export const config = {
+//   runtime: 'edge',
+// };
 
 export default async function handler(req, res) {
   const { tweetID } = req.query;
