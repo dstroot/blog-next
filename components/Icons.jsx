67fcdcfdf7d033b8fa9a7dcfb563592e0c0763e5
Dan Stroot <dan.stroot@gmail.com>
2023-06-10 15:18:56 -0700

    updated dependencies

--- components/Icons.jsx
+++ components/Icons.jsx
@@ -1,7 +1,14 @@
 import { IoLogoJavascript, IoLogoReact, IoLogoHtml5, IoLogoCss3 } from 'react-icons/io5';
 import { VscTerminalBash } from 'react-icons/vsc';
 import { GiEarthAfricaEurope } from 'react-icons/gi';
-import { SiGoogleanalytics, SiGoogletagmanager, SiTailwindcss, SiNextdotjs } from 'react-icons/si';
+import {
+  SiGoogleanalytics,
+  SiGoogletagmanager,
+  SiTailwindcss,
+  SiNextdotjs,
+  SiTypescript,
+} from 'react-icons/si';
+// import { BiLogoTypescript } from 'react-icons/bi';
 import { BsMarkdownFill } from 'react-icons/bs';
 import { CgTimelapse, CgFramer } from 'react-icons/cg';
 
@@ -14,6 +21,8 @@ export const icons = {
   fm: <CgFramer />,
   tailwind: <SiTailwindcss />,
   js: <IoLogoJavascript />,
+  ts: <SiTypescript />,
+  // ts: <BiLogoTypescript />,
   react: <IoLogoReact />,
   html: <IoLogoHtml5 />,
   css: <IoLogoCss3 />,
