02dfb26f72601494bffc54f3ec2333de3a4cb5d8
Dan Stroot <dan.stroot@gmail.com>
2023-08-14 09:32:34 -0700

    working - all posts

--- components/IPhone.jsx
+++ components/IPhone.jsx
@@ -8,7 +8,7 @@ export const IPhone = () => {
   return (
     <>
       {biggerThan500 && (
-        <div className="h-[970px] w-[480px] bg-contain bg-no-repeat bg-[url('/assets/blog/img/iphone-x.png')] m-0 grid place-content-center">
+        <div className="h-[970px] w-[480px] bg-contain bg-no-repeat bg-[url('https://danstroot.imgix.net/assets/blog/img/iphone-x.png')] m-0 grid place-content-center">
           <iframe src={URL} className='h-[800px] w-[395px] -mt-8'></iframe>
         </div>
       )}
